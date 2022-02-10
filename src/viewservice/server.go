ca181ba6b3778d5547e6372ffe0e232620047c32
Taku Takamatsu <59686013+taku-takamatsu@users.noreply.github.com>
2022-02-10 03:04:45 -0500

    passed all test cases -- still messy

--- src/viewservice/server.go
+++ src/viewservice/server.go
@@ -1,44 +1,92 @@
-
 package viewservice
 
-import "net"
-import "net/rpc"
-import "log"
-import "time"
-import "sync"
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
 
 type ViewServer struct {
-  mu sync.Mutex
-  l net.Listener
-  dead bool
-  me string
+	mu   sync.Mutex
+	l    net.Listener
+	dead bool
+	me   string
 
+	// Your declarations here.
+	servers      map[string]time.Time
+	currentView  View
+	primaryAcked bool
+	idle         chan string //keep track of idle servers
+}
 
-  // Your declarations here.
+// helper as we update the view many times...
+func (vs *ViewServer) NewView(p string, b string) {
+	// doesn't need lock as calling functions all use mutex
+	vs.currentView.Viewnum++
+	vs.currentView.Primary = p
+	vs.currentView.Backup = b
+	vs.primaryAcked = false
 }
 
 //
 // server Ping RPC handler.
 //
 func (vs *ViewServer) Ping(args *PingArgs, reply *PingReply) error {
+	vs.mu.Lock()
+	defer vs.mu.Unlock()
 
-  // Your code here.
+	address := args.Me
+	viewNum := args.Viewnum
 
-  return nil
+	fmt.Println("Ping:", address, viewNum)
+	vs.servers[address] = time.Now() //update time
+
+	// Ping(0): new/restarted server
+	if viewNum == 0 {
+		if vs.currentView.Primary == "" { // First primary
+			fmt.Println("Setting primary")
+			vs.NewView(address, "")
+		} else if vs.currentView.Backup == "" { // First backup
+			fmt.Println("Setting backup")
+			vs.NewView(vs.currentView.Primary, address)
+		} else if vs.currentView.Primary == address { //Primary crashed, restarted
+			fmt.Println("Primary crashed and restarted")
+			//primary crashed, promote backup and set old primary as new backup
+			// Reference: https://edstem.org/us/courses/19078/discussion/1101378
+			if vs.primaryAcked { // only update View if Acked
+				vs.NewView(vs.currentView.Backup, address)
+			}
+			fmt.Println("Promoted Backup:", vs.currentView.Primary, vs.currentView.Backup)
+		} else if vs.currentView.Primary != address && vs.currentView.Backup != address {
+			// primary and backup filled, insert new server to idle channel
+			fmt.Println("Idle server")
+			go func() { vs.idle <- address }()
+		}
+	} else if viewNum == vs.currentView.Viewnum && vs.currentView.Primary == address {
+		//acked
+		fmt.Println(viewNum, "ACKED")
+		vs.primaryAcked = true
+	}
+
+	reply.View = vs.currentView
+	fmt.Println("REPLY:", reply.View.Primary, reply.View.Backup, reply.View.Viewnum)
+	return nil
 }
 
-// 
+//
 // server Get() RPC handler.
 //
 func (vs *ViewServer) Get(args *GetArgs, reply *GetReply) error {
-
-  // Your code here.
-
-  return nil
+	// Responds with current view
+	vs.mu.Lock()
+	defer vs.mu.Unlock()
+	reply.View = vs.currentView
+	return nil
 }
-
 
 //
 // tick() is called once per PingInterval; it should notice
@@ -46,8 +94,48 @@ func (vs *ViewServer) Get(args *GetArgs, reply *GetReply) error {
 // accordingly.
 //
 func (vs *ViewServer) tick() {
+	vs.mu.Lock()
+	defer vs.mu.Unlock()
+	latestPrimaryTime := vs.servers[vs.currentView.Primary]
+	if vs.primaryAcked && latestPrimaryTime.Before(time.Now().Add(time.Duration(-PingInterval)*DeadPings)) {
+		fmt.Println("PRIMARY DEAD", vs.currentView.Primary, vs.currentView.Viewnum)
+		//promote backup to primary - akin to New View
+		vs.NewView(vs.currentView.Backup, "")
+		vs.servers[vs.currentView.Primary] = time.Now() // update time
 
-  // Your code here.
+		select {
+		case idle := <-vs.idle:
+			if idle != vs.currentView.Backup && idle != vs.currentView.Primary {
+				fmt.Println("Pulling from idle server")
+				vs.currentView.Backup = idle
+				vs.servers[vs.currentView.Backup] = time.Now()
+			}
+		default:
+			delete(vs.servers, vs.currentView.Backup) // delete time
+			vs.currentView.Backup = ""
+
+			fmt.Println("Promoting Backup to Primary", vs.currentView.Viewnum, vs.currentView.Primary, vs.currentView.Backup)
+		}
+	}
+	latestBackupTime, exist := vs.servers[vs.currentView.Backup]
+	// Promoting backup is akin to setting up new View -- we can't do this until primary is acked
+	if exist && vs.primaryAcked && latestBackupTime.Before(time.Now().Add(time.Duration(-PingInterval)*DeadPings)) {
+		fmt.Println("BACKUP DEAD", vs.currentView.Backup, vs.currentView.Viewnum)
+		select {
+		case idle := <-vs.idle:
+			if idle != vs.currentView.Backup && idle != vs.currentView.Primary {
+				fmt.Println("Pulling from idle server")
+				vs.currentView.Backup = idle
+				vs.servers[vs.currentView.Backup] = time.Now()
+			}
+		default:
+			delete(vs.servers, vs.currentView.Backup) // delete time
+			vs.currentView.Backup = ""
+
+			fmt.Println("Promoting Idle to Backup", vs.currentView.Viewnum, vs.currentView.Primary, vs.currentView.Backup)
+		}
+		vs.primaryAcked = false
+	}
 }
 
 //
@@ -56,54 +144,58 @@ func (vs *ViewServer) tick() {
 // please don't change this function.
 //
 func (vs *ViewServer) Kill() {
-  vs.dead = true
-  vs.l.Close()
+	vs.dead = true
+	vs.l.Close()
 }
 
 func StartServer(me string) *ViewServer {
-  vs := new(ViewServer)
-  vs.me = me
-  // Your vs.* initializations here.
+	vs := new(ViewServer)
+	vs.me = me
+	// Your vs.* initializations here.
+	vs.currentView = View{0, "", ""}
+	vs.servers = make(map[string]time.Time)
+	vs.primaryAcked = false
+	vs.idle = make(chan string, 5) // buffered
 
-  // tell net/rpc about our RPC server and handlers.
-  rpcs := rpc.NewServer()
-  rpcs.Register(vs)
+	// tell net/rpc about our RPC server and handlers.
+	rpcs := rpc.NewServer()
+	rpcs.Register(vs)
 
-  // prepare to receive connections from clients.
-  // change "unix" to "tcp" to use over a network.
-  os.Remove(vs.me) // only needed for "unix"
-  l, e := net.Listen("unix", vs.me);
-  if e != nil {
-    log.Fatal("listen error: ", e);
-  }
-  vs.l = l
+	// prepare to receive connections from clients.
+	// change "unix" to "tcp" to use over a network.
+	os.Remove(vs.me) // only needed for "unix"
+	l, e := net.Listen("unix", vs.me)
+	if e != nil {
+		log.Fatal("listen error: ", e)
+	}
+	vs.l = l
 
-  // please don't change any of the following code,
-  // or do anything to subvert it.
+	// please don't change any of the following code,
+	// or do anything to subvert it.
 
-  // create a thread to accept RPC connections from clients.
-  go func() {
-    for vs.dead == false {
-      conn, err := vs.l.Accept()
-      if err == nil && vs.dead == false {
-        go rpcs.ServeConn(conn)
-      } else if err == nil {
-        conn.Close()
-      }
-      if err != nil && vs.dead == false {
-        fmt.Printf("ViewServer(%v) accept: %v\n", me, err.Error())
-        vs.Kill()
-      }
-    }
-  }()
+	// create a thread to accept RPC connections from clients.
+	go func() {
+		for vs.dead == false {
+			conn, err := vs.l.Accept()
+			if err == nil && vs.dead == false {
+				go rpcs.ServeConn(conn)
+			} else if err == nil {
+				conn.Close()
+			}
+			if err != nil && vs.dead == false {
+				fmt.Printf("ViewServer(%v) accept: %v\n", me, err.Error())
+				vs.Kill()
+			}
+		}
+	}()
 
-  // create a thread to call tick() periodically.
-  go func() {
-    for vs.dead == false {
-      vs.tick()
-      time.Sleep(PingInterval)
-    }
-  }()
+	// create a thread to call tick() periodically.
+	go func() {
+		for vs.dead == false {
+			vs.tick()
+			time.Sleep(PingInterval)
+		}
+	}()
 
-  return vs
+	return vs
 }
